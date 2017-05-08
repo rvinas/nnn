Correct mistake in backpropagation algorithm

nnn/core/layers.py
@@ -58,7 +58,9 @@ class Dense(Layer):
         """
         assert len(der_loss_wrt_act) == self.output_dim
         der_h_wrt_w = np.repeat(np.expand_dims(self.input, axis=0), self.output_dim, axis=0)
-        der_loss_wrt_w = np.expand_dims(der_loss_wrt_act * self.der_act_wrt_h, axis=-1) * der_h_wrt_w
-        backward = np.dot(der_loss_wrt_act, self.weights[:, 1:])  # computes the loss to be transmitted to the previous layer (note that bias term is being excluded)
+        aux = der_loss_wrt_act * self.der_act_wrt_h
+        der_loss_wrt_w = np.expand_dims(aux, axis=-1) * der_h_wrt_w
+        backward = np.dot(aux, self.weights[:, 1:])  # computes the loss to be transmitted to the previous layer (note that bias term is being excluded)
         self.weights -= lr * der_loss_wrt_w
         return np.squeeze(backward)
+

nnn/examples/a_aprox_b.py
@@ -21,7 +21,7 @@ from nnn.core.neural_network import NeuralNetwork
 from nnn.core.objectives import CrossEntropy
 from nnn.utils.plots import plot_boundaries_2d, plot_data
 import numpy as np
-np.random.seed(15) # Note that the neural network is highly sensitive to initial conditions.
+np.random.seed(27) # Note that the neural network is highly sensitive to initial conditions.
 
 n_samples_train = 100
 n_samples_test = 25
@@ -36,7 +36,7 @@ nn = NeuralNetwork(input_dim=2, output_dim=1)
 # Try commenting the next line: the neural network won't be able to classify the samples, because data isn't linearly separable. Thus, we need a hidden layer.
 nn.add_dense(output_dim=2, activation=Sigmoid(), initializer=GaussianInitializer(gaussian_mean=0.0, gaussian_std=1))
 nn.add_dense(output_dim=1, activation=Sigmoid(), initializer=GaussianInitializer(gaussian_mean=0.0, gaussian_std=1))
-nn.train(x=x_train, y=y_train, objective=CrossEntropy(), epochs=25, lr=0.3)
+nn.train(x=x_train, y=y_train, objective=CrossEntropy(), epochs=100, lr=0.3)
 
 print('Train data')
 for i in range(n_samples_train):

nnn/examples/a_greater_than_b.py
@@ -23,7 +23,7 @@ from nnn.utils.plots import plot_boundaries_2d, plot_data, plot_loss_function_3d
 from nnn.utils.loss_grid import loss_grid_nn
 import numpy as np
 
-np.random.seed(15)
+np.random.seed(27)
 
 n_samples_train = 100
 n_samples_test = 25
@@ -36,7 +36,7 @@ plot_data(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, init=0
 
 nn = NeuralNetwork(input_dim=2, output_dim=1)
 nn.add_dense(output_dim=1, activation=Sigmoid(), initializer=GaussianInitializer(gaussian_mean=0.0, gaussian_std=0.3))
-nn.train(x=x_train, y=y_train, objective=CrossEntropy(), epochs=25, lr=0.3)
+nn.train(x=x_train, y=y_train, objective=CrossEntropy(), epochs=100, lr=0.3)
 
 print('Train data')
 for i in range(n_samples_train):